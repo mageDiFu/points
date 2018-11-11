3f37c5b1f6e1e892f772db53474b1d51d6fd0001
mageDiFu <461210108@qq.com>
2018-11-11 16:06:05 -0600

    Add files via upload

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('D:\\Data Management\\final\\mysite\\')
+sys.path.append('D:\\Data Management\\final\\mysite\\mysite')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
